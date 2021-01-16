19f66b05038ca3d643ca073505294fff90744a9e
Grzybek <grzybekdawid@gmail.com>
2021-01-16 10:29:38 +0000

    [init] - initial project commit

---
+++ src/main/java/pl/sda/javalondek4/java_demo/HelloYou.java
@@ -0,0 +1,6 @@
+package pl.sda.javalondek4.java_demo;
+public class HelloYou {
+    public static void main(String[] args) {
+        System.out.println("Witajcie na zajęciach u mańka:)");
+    }
+}
