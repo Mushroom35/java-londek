9682710e1e8c458be1b382c5b483732b52958885
Grzybek <grzybekdawid@gmail.com>
2021-01-16 10:21:38 +0000

    s

---
+++ src/main/java/Test.java
@@ -0,0 +1,6 @@
+// never ever use default package!!!! You cannot import such class!!!
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Wrong hello world!!!");
+    }
+}
