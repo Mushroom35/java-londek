b7b14e6e32e7c30265fc787d6b84848a4da275c5
Grzybek <grzybekdawid@gmail.com>
2021-01-17 11:15:33 +0000

    [change] - first exercises with lambdas

--- src/main/java/pl/sda/javalondek4/java_demo/lambda/HelloLambda.java
+++ src/main/java/pl/sda/javalondek4/java_demo/lambda/HelloLambda.java
@@ -16,12 +16,33 @@ public class HelloLambda {
         };
         handleRunner(anonymousRunner);
 
+        // call handleRunner user anonymous class
         handleRunner(new Runner() {
             @Override
             public void go() {
                 System.out.println("inside method call");
             }
         });
+
+        //using lambda - supper happy :)
+        handleRunner(() -> System.out.println("inside lambda call"));
+
+        //we're implemetning method go() here!!!
+        Runner myFirstLambda = () -> {
+        };
+        //expression has value, statement not (void)
+        Runner mySecondLambda = () -> {
+            System.out.println("Inside my second lambda");
+        };
+        Runner myThirdLambda = () -> System.out.println("my third lambda");
+
+        Runner myFourthLambda = () -> {
+            System.out.println("first text");
+            System.out.println("second text");
+        };
+        myFourthLambda.go();
+        myFourthLambda.go(4);
+        Runner.walk();
     }
 
     public static void handleRunner(Runner anyRunner) {
