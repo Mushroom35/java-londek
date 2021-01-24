a53447da5fe00469f87907b642c10e3ad15e2061
Grzybek <grzybekdawid@gmail.com>
2021-01-24 12:08:07 +0000

    [change] - playing with Optional<> class

--- src/main/java/pl/sda/javalondek4/java_demo/optional/MySecret.java
+++ src/main/java/pl/sda/javalondek4/java_demo/optional/MySecret.java
@@ -1,21 +1,23 @@
 package pl.sda.javalondek4.java_demo.optional;
 
-import pl.sda.javalondek4.java_demo.lambda.calculator.MyAdder;
+import pl.sda.javalondek4.java_demo.lambda.calculator.Operation;
+
+import java.util.Optional;
 
 import static java.util.Objects.nonNull;
 
-public class MySecret {
-    private String secret;
+public class MySecret <T> {
+    private T secret;
 
-    public MySecret(String secret) {
+    public MySecret(T secret) {
         this.secret = secret;
     }
 
-    public String getSecret() {
+    public T getSecret() {
         return secret;
     }
 
-    public void setSecret(String secret) {
+    public void setSecret(T secret) {
         this.secret = secret;
     }
 
@@ -27,13 +29,24 @@ public class MySecret {
     }
 
     public static void main(String[] args) {
-        MySecret realOne = new MySecret("I like learning JVM technologies :P");
-        MySecret withNullContent = new MySecret(null);
+        MySecret<String> realOne = new MySecret<>("I like learning JVM technologies :P");
+        MySecret<String> withNullContent = new MySecret<>(null);
         printSecretLenght(realOne);
         printSecretLenght(withNullContent);
+
+        MySecret<Integer> secretNumber = new MySecret<>(42);
+        MySecret<Integer> secretNullNumber = new MySecret<>(null);
+        var secretNumOne = secretNumber.getSecret(); // 42
+        var secretNull = secretNullNumber.getSecret(); // null
+
+        // Optional<Integer> // 42 and null
+        Optional<Integer> number = Optional.of(42); // never ever null inside
+        Optional<Integer> nullNumber = Optional.empty();
+        Optional<Integer> nullNumber2 = Optional.ofNullable(null);
+//        Optional<Integer> nullNumber3 = Optional.of(null); // dont't do it at home :)
     }
 
-    public static void printSecretLenght(MySecret mySecret) {
+    public static void printSecretLenght(MySecret<String> mySecret) {
         int secretLen = mySecret.getSecret() != null ? mySecret.getSecret().length() : 0;
         secretLen = nonNull(mySecret.getSecret()) ? mySecret.getSecret().length() :0;
 //        the same functionality
@@ -45,4 +58,25 @@ public class MySecret {
         System.out.println("My secret has " + secretLen + " characters");
     }
 
+    public static <T> void processSecret(T optional) {
+        // make optional from T
+        Optional<T> opt = Optional.ofNullable(optional);
+        int number = 4;
+        Optional<Integer> numberInsideOptional = Optional.of(number);
+
+//        T unpacked = opt.get(); // tak nie robić
+        T unpacked;
+        if (opt.isPresent()) {
+            unpacked = opt.get();
+        } else {
+            unpacked = null;
+        }
+
+        unpacked = opt.orElse(null);
+        unpacked = (T) opt.or(() -> null);
+
+        Optional<String> string = Optional.empty();
+        Optional<String> fromOptional = string.or(() -> Optional.of("no value"));
+
+    }
 }
